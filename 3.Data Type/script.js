f638bcb1bcba5ed0b93d179026d7179cca539b35
ankit9600 <ankitmor1811@gmail.com>
2026-05-14 10:03:11 +0530

    added 3.Data Type

---
+++ 3.Data Type/script.js
@@ -0,0 +1,76 @@
+// Data Type : Data Type Is a define value type of data
+
+// data type in use java script two types
+
+// 1.primitive , 2.Non premitive
+
+// 1. primitive Data type :
+
+// 1.number:-
+
+const num = 10;
+
+console.log(typeof num);
+
+console.log("This is number data type:",num);
+
+//2.string :-
+
+const name = "ankit";
+
+console.log(typeof name);
+
+console.log("This is string data type:",name);
+
+// 3.boolean :-
+
+const Data = true;
+
+console.log(typeof Data);
+
+console.log("This is boolean data type:",Data);
+
+// 4.bigInt :-
+
+const number = 12123413435345;
+
+console.log(typeof number);
+
+console.log("This is bigInt data type:",number);
+
+// 5.null :-
+
+const value = null;
+
+console.log(typeof value);
+
+console.log("This is null data type:",value);
+
+// 6. symbol :-
+
+var data = 10;
+
+var data = 20;
+
+console.log(typeof data);
+
+console.log("This is symbole data type:",data);
+
+// 7. undefined :-
+
+var x;
+
+console.log(typeof a);
+
+console.log("This is undefined data type:",a);
+
+
+// 2. Non-primitive dat type :
+
+// 8. object :-
+
+const array = {name : "ankit"};
+
+console.log(typeof array);
+
+console.log("This is number data type:",array);
