3349ee9fd6c95d212ce460c12eb0e4418955764a
ankit9600 <ankitmor1811@gmail.com>
2026-05-27 13:09:48 +0530

    added new

--- 6.string/1.script.js
+++ 6.string/1.script.js
@@ -19,3 +19,35 @@ console.log(name.includes("m")); // Boolean
 // concat :-
 
 console.log(name.concat(" ", "age is 20")); 
+
+// indexof :-
+
+console.log(name.indexOf("mor"));
+
+//last indexOf :-
+
+console.log(name.lastIndexOf("t"));
+
+// length :-
+
+console.log(name.length);
+
+// match :-
+
+console.log(name.match(""));
+
+// repeat :-
+
+console.log(name.repeat("5"));
+
+// repleace :-
+
+console.log(name.replace("i","I"));
+
+// search :-
+
+console.log(name.search("a"));
+
+// slice :-
+
+console.log(name.slice(2,6));
