284ac0550bbb8bc1b294ae8ef6ed517ef67609e5
ankit9600 <ankitmor1811@gmail.com>
2026-06-05 13:12:20 +0530

    add array loop

---
+++ 13.array/4.array.js
@@ -0,0 +1,33 @@
+// (1) for of method :=
+
+let cities = ["delhi", "pune", "mumbai", "hydrabad", "gurgaon"];
+
+for (let city of cities) {
+
+    console.log(city);
+
+}
+
+// for each methode :=
+
+let fruits = [
+    "apple",
+    "banana",
+    "cherry",
+    "mango",
+    "guava",
+    "strawberry",
+    "pineapple",
+];
+
+fruits.forEach(function (fruits) {
+    console.log("fruits is :", fruits);
+});
+
+// for :-
+
+for(i=0;i<fruits.length;i++){
+    fruits[i];
+}
+
+console.log(fruits);
