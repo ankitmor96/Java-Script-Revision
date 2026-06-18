7c1a28a67c0b6d6bb3068cea54f113a65fe551be
ankit9600 <ankitmor1811@gmail.com>
2026-06-18 11:54:29 +0530

    add async setTimeOut

---
+++ async/setTimeOut.js
@@ -0,0 +1,13 @@
+function StartServer(){
+
+    setTimeout(function StartServer(){
+
+        console.log("5000");
+
+    }, 2000);
+
+    console.log("server is runing on port");
+
+};
+
+StartServer();
